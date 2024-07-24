0db14dbf7a39ba3bda447a1913a7c962fdbd162c
Your Name <you@example.com>
2024-07-24 10:46:36 -0300

    😂👍

--- Looking-Glass/src/App.js
+++ Looking-Glass/src/App.js
@@ -2,27 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import logo from './logo.png';
-
-function Selector() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="description">
-          <span className="text">Tipo de Consulta</span>
-          <select className="option" value='' onChange=''>
-            <option value="ping">Ping</option>
-            <option value="as">AS</option>
-            <option value="whois">Whois</option>
-          </select>
-        </div>
-        <div className="description">
-          <span className="text">Dados da Consulta</span>
-          <input type="text" className="input" placeholder="IP Address, AS, Prefix" value='' onChange='' onKeyPress='' />
-        </div>
-      </div>
-    </div>
-  );
-}
+import Selector from './Selector';
 
 function App() {
   const [showSelector, setShowSelector] = useState(false);
@@ -37,7 +17,7 @@ function App() {
           <Button className="button" onClick={toggleSelector}>São Paulo, IX <span className="circle">BR</span></Button>
           <Button className="button" onClick={toggleSelector}>New York, AT&T <span className="circle">US</span></Button>
         </div>
-        {showSelector && <Selector />} {/* Passo 4 */}
+        {showSelector && <Selector />}
       </header>
     </div>
   );
