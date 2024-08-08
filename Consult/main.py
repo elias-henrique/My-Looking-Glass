cd537bafe020e73f33c4632850565c397eadbf7b
Your Name <you@example.com>
2024-08-08 15:13:27 -0300

    😂👍

---
+++ Consult/main.py
@@ -0,0 +1,84 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException
+from typing import Dict, Any
+import subprocess
+import requests
+
+from bgphe import (
+    extract_irr_data,
+    extract_info,
+    extract_network_info,
+    extract_whois_data
+)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+BASE_URL = 'https://bgp.he.net/'
+
+
+def fetch_data_he(address: str = '') -> str:
+    """Faz a requisição HTTP e retorna o texto HTML."""
+    if address:
+        if '/' in address:
+            url = f'{BASE_URL}net/{address}'
+        else:
+            url = f'{BASE_URL}ip/{address}'
+    else:
+        url = BASE_URL
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        raise HTTPException(
+            status_code=400, detail=f"Error fetching data from {url}: {e}")
+
+    return response.text
+
+
+@app.get('/whois')
+def whois(address: str = None) -> Dict[str, Any]:
+    try:
+        whois = subprocess.getoutput(f'whois {address}')
+        return  extract_whois_data(whois)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {e}")
+
+
+@app.get('/')
+def meu_ip() -> Dict[str, Any]:
+    try:
+        html_content = fetch_data_he()
+        info = extract_info(html_content)
+
+        return {'info': info}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {e}")
+
+
+@app.get('/ping')
+def ping(data: str) -> Dict[str, Any]:
+    try:
+        types,  address = data.split()
+        result = subprocess.getoutput(f'ping -c 5 -{types} {address}')
+
+        ping_result = result.split('\n')
+        return {'result': ping_result}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {e}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- Looking-Glass/src/Selector.js
+++ Looking-Glass/src/Selector.js
@@ -4,15 +4,16 @@ import './App.css';
 function ASInfo({ asInfo }) {
     return (
         <div className="as-info-card">
-            <h2 className="as-info-title">{asInfo.name}</h2>
+            <h2 className="as-info-title">{asInfo.Owner || "Not available"} - {asInfo.Country && asInfo.Country}</h2>
             <div className="as-info-content">
-                <p><strong>ASN:</strong> {asInfo.asn}</p>
-                <p><strong>Descrição:</strong> {asInfo.description_short}</p>
-                <p><strong>País:</strong> {asInfo.country_code}</p>
-                <p><strong>Website:</strong> <a href={asInfo.website}>{asInfo.website}</a></p>
-                <p><strong>Looking Glass:</strong> <a href={asInfo.looking_glass}>{asInfo.looking_glass}</a></p>
-                <p><strong>Estimativa de Tráfego:</strong> {asInfo.traffic_estimation}</p>
-                <p><strong>Relação de Tráfego:</strong> {asInfo.traffic_ratio}</p>
+                {asInfo.Responsible && <p><strong>Responsible:</strong> {asInfo.Responsible}</p>}
+                {asInfo.CIDR_v4 && <p><strong>Net Range (v4):</strong> {asInfo.CIDR_v4}</p>}
+                {asInfo.CIDR_v6 && <p><strong>Net Range (v6):</strong> {asInfo.CIDR_v6}</p>}
+                {asInfo.Ownerid && <p><strong>CNPJ:</strong> {asInfo.Ownerid}</p>}
+                {asInfo['e-mail'] && <p><strong>Notify:</strong> {asInfo['e-mail']}</p>}
+                {asInfo.OriginAS && <p><strong>OriginAS:</strong> {asInfo.OriginAS}</p>}
+                {asInfo.changed && <p><strong>Changed:</strong> {asInfo.changed}</p>}
+                {asInfo.created && <p><strong>Created:</strong> {asInfo.created}</p>}
             </div>
         </div>
     );
@@ -30,44 +31,6 @@ function PingData({ pingData }) {
                     </div>
                 ))}
             </div>
-        </div>
-    );
-}
-
-function WhoisData({ whoisData, irrData }) {
-    return (
-        <div className="as-info-card">
-            <h2 className="as-info-title">{whoisData.owner}</h2>
-            <div className="as-info-section">
-                <div className="as-info-content">
-                    <p><strong>Net Range:</strong> {whoisData.NetRange}</p>
-                    <p><strong>CIDR:</strong> {whoisData.inetnum}</p>
-                    <p><strong>ASN:</strong> {whoisData['aut-num']}</p>
-                    <p><strong>Address:</strong> {whoisData.Address}</p>
-                    <p><strong>City:</strong> {whoisData.City} - <strong>{whoisData.Country}</strong></p>
-                    <p><strong>CNPJ:</strong> {whoisData.ownerid}</p>
-                    <p><strong>Responsible:</strong> {whoisData.person}</p>
-                </div>
-            </div>
-            {irrData && (
-                <div className="as-info-section">
-                    <div className="as-info-content">
-                        {irrData.map((data, index) => (
-                            <div key={index} className="irr-info-item">
-                                <p><strong>Source:</strong> {data.source}</p>
-                                <p><strong>Route:</strong> {data.route}</p>
-                                <p><strong>Description:</strong> {data.descr}</p>
-                                <p><strong>Origin:</strong> {data.origin}</p>
-                                <p><strong>Member Of:</strong> {data['member-of']}</p>
-                                <p><strong>Notify:</strong> {data.notify}</p>
-                                <p><strong>Maintained By:</strong> {data['mnt-by']}</p>
-                                <p><strong>Changed:</strong> {data.changed}</p>
-                                <p><strong>Last Modified:</strong> {data['last-modified']}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )}
         </div>
     );
 }
@@ -112,10 +75,8 @@ function Selector() {
         event.preventDefault();
         setResult(null);
         console.log(selectedOption);
-        if (selectedOption === 'as') {
-            await fetchData(`http://0.0.0.0:8000/net?asn=${inputValue}`);
-        } else if (selectedOption === 'whois') {
-            await fetchData(`http://0.0.0.0:8000/net?address=${inputValue}`);
+        if (selectedOption === 'whois') {
+            await fetchData(`http://0.0.0.0:8000/whois?address=${inputValue}`);
         } else if (selectedOption === 'ping') {
             await fetchData(`http://0.0.0.0:8000/ping?data=${inputValue}`);
         }
@@ -130,7 +91,6 @@ function Selector() {
                             <span className="text">Tipo de Consulta</span>
                             <select className="option" value={selectedOption} onChange={handleChangeSelect}>
                                 <option value="ping">Ping</option>
-                                <option value="as">AS</option>
                                 <option value="whois">Whois</option>
                             </select>
                         </div>
@@ -150,8 +110,7 @@ function Selector() {
             {isLoading && <div className="loading">Carregando...</div>}
             {result && (
                 <div className="card">
-                    {selectedOption === 'as' && result && <ASInfo asInfo={result} />}
-                    {selectedOption === 'whois' && result && <WhoisData whoisData={result.whois_data} irrData={result.irr_data} />}
+                    {selectedOption === 'whois' && result && <ASInfo asInfo={result} />}
                     {selectedOption === 'ping' && result && <PingData pingData={result.result} />}
                 </div>
             )}
